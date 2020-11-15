0611b4e008cc2c9cc93cd556b3cdad6b0133f4cf
Gavin McCabe <gavin@gavinmccabe.com>
2020-11-15 14:40:27 -0500

    Finish mini_project_1 assignment sheet

---
+++ mini_project_1.swift
@@ -0,0 +1,20 @@
+/**
+ 
+ # Mini Project 1
+ 
+  Today, you'll be creating a `Vehicle` `struct`.  The `struct` should store information on the
+   vehicle's make, model, color, gas milage, type (truck, sudan, etc.), and current fuel level.
+ 
+  The `struct` should include methods that:
+ 
+   - Fill the gas tank up completely
+   - Drive a certain number of miles (if it has enough fuel), and deducting the fuel accordingly after the drive.
+     The vehicle will not drive if it doesn't have enough fuel (and will print an error message).
+ 
+   As part of the project, I'll be looking for :
+    
+    - Proper use of git branches and merge requests
+    - Proper documentation
+    - Proper code style
+ 
+ */
