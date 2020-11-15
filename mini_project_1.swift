d8b4dfee4fc4dcf4bf208958a37f757af4a7aba7
Jminding <jtding43@gmail.com>
2020-11-15 15:40:00 -0500

    yeee

--- mini_project_1.swift
+++ mini_project_1.swift
@@ -3,7 +3,7 @@
  # Mini Project 1
  
   Today, you'll be creating a `Vehicle` `struct`.  The `struct` should store information on the
-   vehicle's make, model, color, gas milage, type (truck, sudan, etc.), total capacity of the tank, and current fuel level.
+   vehicle's make, model, color, gas milage, type (truck, sedan, etc.), total capacity of the tank, and current fuel level.
  
   The `struct` should include methods that:
  
@@ -19,16 +19,18 @@
  
  */
 
+/** This represents a vehicle. **/
 struct Vehicle {
     
-    var make: String
-    var model: String
-    var color: String
-    var gasMileage: Double
-    var type: String
-    var capacity: Double
-    var currentFuelLevel: Double
+    var make: String // make of vehicle
+    var model: String // model of vehicle
+    var color: String // vehicle's color
+    var gasMileage: Double // the gas mileage of the vehicle in mpg
+    var type: String // type of vehicle
+    var capacity: Double // total fuel capacity in gallons
+    var currentFuelLevel: Double // current fuel level in gallons
     
+    /// Fills the gas tank up to match its capacity
     mutating func fillTankUp() {
         
         currentFuelLevel = capacity
@@ -36,26 +38,22 @@ struct Vehicle {
         
     }
     
+    /// Drives a certain number of miles if possible, updates fuel level if it has enough, otherwise it returns the current fuel level and does not complete the trip
+    /// - Parameter miles: miles driven
+    /// - Returns: remaining amount of fuel
     mutating func drive(miles: Double) -> Double {
         
-        var gallonsNeeded = miles/gasMileage
+        var gallonsNeeded = miles / gasMileage
         if gallonsNeeded > currentFuelLevel {
             
             print("Not enough fuel!")
             return currentFuelLevel
             
-        }
-        else {
+        } else {
             
             print("Sure")
             currentFuelLevel = currentFuelLevel - gallonsNeeded
             return currentFuelLevel
         }
-        
-        
-        
     }
-    
-    
-    
 }
