cc4dab32f6627dd4141a7add78f97cf3575bbc7a
Jminding <jtding43@gmail.com>
2020-11-18 18:41:46 -0500

    e

--- Vehicle.swift
+++ Vehicle.swift
@@ -11,16 +11,47 @@
  */
 
 
+/// Represents a vehicle
 struct Vehicle {
     
+    var make: String /// the make of the vehicle
+    var model: String /// the model of the vehicle
+    var color: String /// the color of the vehicle
+    var type: String /// the type of the vehicle
+    var numDoors: Int /// the number of doors the vehicle has
     
-    var make: String
-    var model: String
-    var color: String
-    var type: String
-    var numDoors: Int
+    init(make: String, model: String, color: String, numDoors: Int) {
+        
+        self.make = make
+        self.model = model
+        self.color = color
+        self.type = type
+        self.numDoors =  numDoors
+        
+    }
     
+    /// Plays a song of your choice
+    /// - Parameter song: The song to be played
+    func playSong(song: String) {
+        
+        print("Playing \(song)!")
+        
+    }
     
+    /// Drives the vehicle
+    /// - Parameter miles: the number of miles to be driven
+    func drive(miles: Double) {
+        
+        print("Driving \(miles)!")
+        
+    }
+    
+    /// Honks at another vehicle
+    func honk() {
+        
+        print("MOVE ALREADY @#$@%#$%@#!")
+        
+    }
     
     
 }
