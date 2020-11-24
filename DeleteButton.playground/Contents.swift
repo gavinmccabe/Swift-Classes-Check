50bb25af312a3098dc5e0dac57c1a217dc39b279
Jminding <jtding43@gmail.com>
2020-11-24 17:59:10 -0500

    edits

--- DeleteButton.playground/Contents.swift
+++ DeleteButton.playground/Contents.swift
@@ -1,15 +1,21 @@
 import SwiftUI
+import PlaygroundSupport
 
 struct DeleteButton: View {
     
     var body: some View {
         
         ZStack(){
+            RoundedRectangle(cornerRadius: 5)
+                .foregroundColor(Color(#colorLiteral(red: 0.06073485315, green: 0.0193178039, blue: 0.2020891309, alpha: 1)))
+                .frame(width: 100, height: 60)
             HStack(){
-                Image(systemName: trash)
+                Image(systemName: "trash")
+                    .foregroundColor(.white)
+                    .font(.custom("Arial",size: 20.0))
                 Text("Delete")
+                    .foregroundColor(.white)
             }
-            RoundedRectangle(cornerRadius: 20)
         }
         
         
@@ -17,12 +23,4 @@ struct DeleteButton: View {
 
 }
 
-
-struct DeleteButton_Previews: PreviewProvider {
-    
-    static var previews: some View {
-        
-        DeleteButton()
-    }
-    
-}
+PlaygroundPage.current.setLiveView(DeleteButton())
