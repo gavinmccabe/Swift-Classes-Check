985385d6fac87da70f0cd3a631d29a2b8881b84f
Jminding <jtding43@gmail.com>
2020-11-25 14:34:02 -0500

    I made some more changes
    I added the shadow, spaced my code out a bit more, changed the color up a bit, and tried to get the dimensions to be closer.

--- DeleteButton.playground/Contents.swift
+++ DeleteButton.playground/Contents.swift
@@ -6,18 +6,24 @@ struct DeleteButton: View {
     var body: some View {
         
         ZStack(){
-            RoundedRectangle(cornerRadius: 5)
-                .foregroundColor(Color(#colorLiteral(red: 0.06073485315, green: 0.0193178039, blue: 0.2020891309, alpha: 1)))
-                .frame(width: 100, height: 60)
+            
+            RoundedRectangle(cornerRadius: 7)
+                .foregroundColor(Color(#colorLiteral(red: 0.1450980392, green: 0.1490196078, blue: 0.2156862745, alpha: 1)))
+                .frame(width: 149, height: 50)
+                .shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.1026428811)), radius: 8, x: 0, y: 1)
+            
             HStack(){
+                
                 Image(systemName: "trash")
                     .foregroundColor(.white)
                     .font(.custom("Arial",size: 20.0))
-                Text("Delete")
+                
+                Text("Delete Item")
                     .foregroundColor(.white)
+                
             }
+            
         }
-        
         
     }
 
