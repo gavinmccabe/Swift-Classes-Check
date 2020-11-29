d2cee29a4f750e717952bb379bb079257fb34411
Jminding <jtding43@gmail.com>
2020-11-29 15:10:36 -0500

    New project!

---
+++ doesntmatter/doesntmatter/doesntmatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  doesntmatterApp.swift
+//  doesntmatter
+//
+//  Created by Jaymin Ding on 11/29/20.
+//
+
+import SwiftUI
+
+@main
+struct doesntmatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
