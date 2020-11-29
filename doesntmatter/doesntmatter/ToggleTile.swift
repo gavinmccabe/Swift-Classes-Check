88cb21cb7c73ac9d39385df1023310e6bf95f90e
Jminding <jtding43@gmail.com>
2020-11-29 15:22:52 -0500

    Made a new file ahahahaa

---
+++ doesntmatter/doesntmatter/ToggleTile.swift
@@ -0,0 +1,20 @@
+//
+//  toggletile.swift
+//  doesntmatter
+//
+//  Created by Jaymin Ding on 11/29/20.
+//
+
+import SwiftUI
+
+struct toggletile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct toggletile_Previews: PreviewProvider {
+    static var previews: some View {
+        toggletile()
+    }
+}
