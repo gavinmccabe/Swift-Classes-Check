a91ee06e7d77bdf1141600c31a704a42ef55b842
Jminding <jtding43@gmail.com>
2020-11-29 16:02:17 -0500

    Yes untoggled version

--- doesntmatter/doesntmatter/ToggleTileToggled.swift
+++ doesntmatter/doesntmatter/ToggleTileToggled.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct ToggleTileToggled: View {
+    @State var text: String
+    @State var image: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(){
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(Color(#colorLiteral(red: 0.1764705882, green: 0.462745098, blue: 0.937254902, alpha: 1)))
+            VStack(){
+                Spacer()
+                Image(systemName: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 40, height: 40)
+                    .foregroundColor(.white)
+                Spacer()
+                Text(text)
+                    .font(.custom("HelveticaNeue", size: 18))
+                    .foregroundColor(.white)
+                Spacer()
+            }
+        }.frame(width: 125, height: 125)
     }
 }
 
 struct ToggleTileToggled_Previews: PreviewProvider {
     static var previews: some View {
-        ToggleTileToggled()
+        ToggleTileToggled(text: "Yo wassup", image: "archivebox")
     }
 }
