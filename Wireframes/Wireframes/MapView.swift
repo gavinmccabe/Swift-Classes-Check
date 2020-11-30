1bc9d2d62d50ed061c4644b53ec76af00516f747
Jminding <jtding43@gmail.com>
2020-11-30 15:42:12 -0500

    Made the Rectangles and Map

--- Wireframes/Wireframes/MapView.swift
+++ Wireframes/Wireframes/MapView.swift
@@ -5,15 +5,43 @@
 //  Created by Jaymin Ding on 11/30/20.
 //
 
+import MapKit
 import SwiftUI
-
-struct MapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct isdrycleaner: Identifiable {
+    var id = UUID()
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+      CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
-}
+  }
+struct MapView: View {
+    let drycleanersintenafly = [
+      isdrycleaner(name: "Tenafly Dry Cleaners", latitude: 40.927160, longitude: -73.966750),
+      isdrycleaner(name: "Cambridge Cleaners", latitude:  40.924970, longitude: -73.965060),
+      isdrycleaner(name: "Lowell Cleaners", latitude: 40.927100, longitude: -73.966010)
+    ]
+    @State var coordinateRegion = MKCoordinateRegion(
+      center: CLLocationCoordinate2D(latitude: 40.919800, longitude: -73.960580),
+      span: MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03))
+    var body: some View {
+        if #available(iOS 14.0, *) {
+            
+            ZStack{
+                Map(coordinateRegion: $coordinateRegion,
+                    annotationItems: drycleanersintenafly) { place in
+                    MapMarker(coordinate: place.coordinate, tint: Color(#colorLiteral(red: 1, green: 0, blue: 0, alpha: 1)))
+                }.edgesIgnoringSafeArea(.all)
+            }.edgesIgnoringSafeArea(.all)
 
-struct MapView_Previews: PreviewProvider {
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+  }
+struct View_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
     }
