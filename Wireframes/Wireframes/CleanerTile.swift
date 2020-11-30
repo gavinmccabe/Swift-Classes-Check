4812f2d767667138601af7312a4e93a4bc5f2064
Jminding <jtding43@gmail.com>
2020-11-30 15:32:18 -0500

    Create files

---
+++ Wireframes/Wireframes/CleanerTile.swift
@@ -0,0 +1,20 @@
+//
+//  CleanerTile.swift
+//  Wireframes
+//
+//  Created by Jaymin Ding on 11/30/20.
+//
+
+import SwiftUI
+
+struct CleanerTile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CleanerTile_Previews: PreviewProvider {
+    static var previews: some View {
+        CleanerTile()
+    }
+}
---
+++ Wireframes/Wireframes/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Wireframes
+//
+//  Created by Jaymin Ding on 11/30/20.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
