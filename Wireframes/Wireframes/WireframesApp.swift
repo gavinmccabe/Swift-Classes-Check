04054ff0a5efdf4d5e77881856273a64613ec1d6
Gavin McCabe <gavin@gavinmccabe.com>
2020-11-29 22:02:44 -0500

    Create project

---
+++ Wireframes/Wireframes/WireframesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WireframesApp.swift
+//  Wireframes
+//
+//  Created by Gavin on 11/29/20.
+//
+
+import SwiftUI
+
+@main
+struct WireframesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
