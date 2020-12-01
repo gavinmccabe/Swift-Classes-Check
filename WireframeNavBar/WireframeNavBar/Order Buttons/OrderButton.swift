4e382fdb48f43bb42ebcc35cc6b05841e8f4cfa8
Jminding <jtding43@gmail.com>
2020-11-30 21:16:25 -0500

    Create order buttons

---
+++ WireframeNavBar/WireframeNavBar/Order Buttons/OrderButton.swift
@@ -0,0 +1,20 @@
+//
+//  OrderButton.swift
+//  WireframeNavBar
+//
+//  Created by Jaymin Ding on 11/30/20.
+//
+
+import SwiftUI
+
+struct OrderButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderButton_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderButton()
+    }
+}
