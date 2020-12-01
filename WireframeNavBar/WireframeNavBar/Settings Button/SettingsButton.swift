4d683a9fd8ba32d51de374f054c79cbf79adb1fb
Jminding <jtding43@gmail.com>
2020-11-30 21:24:14 -0500

    Create settings btn

---
+++ WireframeNavBar/WireframeNavBar/Settings Button/SettingsButton.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsButton.swift
+//  WireframeNavBar
+//
+//  Created by Jaymin Ding on 11/30/20.
+//
+
+import SwiftUI
+
+struct SettingsButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsButton()
+    }
+}
