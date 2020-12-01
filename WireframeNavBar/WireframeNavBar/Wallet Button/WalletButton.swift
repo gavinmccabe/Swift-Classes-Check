6def378ad835b49bdc3eb3f02498ed2ed222533c
Jminding <jtding43@gmail.com>
2020-11-30 21:25:31 -0500

    Create wallet button

---
+++ WireframeNavBar/WireframeNavBar/Wallet Button/WalletButton.swift
@@ -0,0 +1,20 @@
+//
+//  WalletButton.swift
+//  WireframeNavBar
+//
+//  Created by Jaymin Ding on 11/30/20.
+//
+
+import SwiftUI
+
+struct WalletButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WalletButton_Previews: PreviewProvider {
+    static var previews: some View {
+        WalletButton()
+    }
+}
---
+++ WireframeNavBar/WireframeNavBar/Wallet Button/WalletButtonToggled.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  WireframeNavBar
+//
+//  Created by Jaymin Ding on 11/30/20.
+//
+
+import SwiftUI
+
+struct WalletButtonToggled: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WalletButtonToggled_Previews: PreviewProvider {
+    static var previews: some View {
+        WalletButtonToggled()
+    }
+}
