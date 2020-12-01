89aafb8c064a6bf0159c62146e244c9c778139bc
Jminding <jtding43@gmail.com>
2020-11-30 21:54:40 -0500

    Nav Bar… One error

---
+++ WireframeNavBar/WireframeNavBar/NavBar.swift
@@ -0,0 +1,42 @@
+//
+//  NavBar.swift
+//  WireframeNavBar
+//
+//  Created by Jaymin Ding on 11/30/20.
+//
+
+import SwiftUI
+
+struct NavBar: View {
+    var body: some View {
+        VStack(){
+            Spacer()
+            ZStack(){
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width, height: 50)
+                HStack(){
+                    
+                    Spacer()
+                    HomeButton()
+                    Spacer()
+                    OrderButton()
+                    Spacer()
+                    SettingsButton()
+                    Spacer()
+                    WalletButton()
+                    Spacer()
+                    PlusButton()
+                    Spacer()
+                    
+                }
+            }
+        }
+    }
+}
+
+struct NavBar_Previews: PreviewProvider {
+    static var previews: some View {
+        NavBar()
+    }
+}
