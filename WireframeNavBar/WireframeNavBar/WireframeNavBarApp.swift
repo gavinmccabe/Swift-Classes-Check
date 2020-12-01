055d9e514b99a9cd13838994d1ac9169f947a096
Jminding <jtding43@gmail.com>
2020-11-30 20:02:35 -0500

    New part of proj!

---
+++ WireframeNavBar/WireframeNavBar/WireframeNavBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WireframeNavBarApp.swift
+//  WireframeNavBar
+//
+//  Created by Jaymin Ding on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct WireframeNavBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
