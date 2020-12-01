5029c8bb5980c996e0c957ee9ebe49436474f473
Jminding <jtding43@gmail.com>
2020-11-30 22:27:19 -0500

    Made some more changes
    What is this weird UserInterfaceState.xcuserstate file?  It keeps popping up after I commit everything :sob:

--- Wireframes/Wireframes/Cleaner Tiles/CleanerTileText.swift
+++ Wireframes/Wireframes/Cleaner Tiles/CleanerTileText.swift
@@ -17,12 +17,16 @@ struct CleanerTileText: View {
             HStack(){
                 VStack(){
                     HStack(){
+                        Spacer()
+                            .frame(width: 5)
                         Text(cleanerName)
                             .foregroundColor(Color(#colorLiteral(red: 0.5803921569, green: 0.6156862745, blue: 0.6549019608, alpha: 1)))
                         Spacer()
                     }
                     Spacer()
                     HStack(){
+                        Spacer()
+                            .frame(width: 5)
                         Text(cleanerAddress)
                             .foregroundColor(Color(#colorLiteral(red: 0.8196078431, green: 0.831372549, blue: 0.8431372549, alpha: 1)))
                             .font(.custom("HelveticaNeue", size: 10))
@@ -30,14 +34,18 @@ struct CleanerTileText: View {
                     }
                 }
                 Spacer()
-                ZStack(){
+                HStack(){
+                    ZStack(){
                     
-                    RoundedRectangle(cornerRadius: 10)
-                        .foregroundColor(Color(#colorLiteral(red: 0.9647058824, green: 0.968627451, blue: 0.9764705882, alpha: 1)))
-                        .frame(width: 30, height: 40)
-                    Image(systemName: "chevron.forward")
-                        .foregroundColor(Color(#colorLiteral(red: 0.6862745098, green: 0.7019607843, blue: 0.7803921569, alpha: 1)))
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color(#colorLiteral(red: 0.9647058824, green: 0.968627451, blue: 0.9764705882, alpha: 1)))
+                            .frame(width: 30, height: 40)
+                        Image(systemName: "chevron.forward")
+                            .foregroundColor(Color(#colorLiteral(red: 0.6862745098, green: 0.7019607843, blue: 0.7803921569, alpha: 1)))
                     
+                    }
+                    Spacer()
+                        .frame(width: 5)
                 }
             }
         }.frame(width: 250, height: 50)
