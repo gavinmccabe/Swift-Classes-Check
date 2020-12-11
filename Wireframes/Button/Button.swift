e15ac4f97fb145748125fa6f1e5100d2fca2b4f2
Jminding <jtding43@gmail.com>
2020-12-11 12:31:44 -0500

    Button

---
+++ Wireframes/Button/Button.swift
@@ -0,0 +1,25 @@
+//
+//  Button.swift
+//  Wireframes
+//
+//  Created by Jaymin Ding on 12/11/20.
+//
+
+import SwiftUI
+
+struct Button: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    func getStartedBtnPressed() {
+        
+        print("Get Started!")
+        
+    }
+}
+
+struct Button_Previews: PreviewProvider {
+    static var previews: some View {
+        Button()
+    }
+}
