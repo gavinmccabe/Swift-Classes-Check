f480e692c36c21b1b592384d5fc85aef58309f27
Jminding <jtding43@gmail.com>
2020-12-13 14:38:34 -0500

    Status Tile for Order Tile

---
+++ Wireframes/Wireframes/Order Tile/HangrRating.swift
@@ -0,0 +1,20 @@
+//
+//  HangrRating.swift
+//  Wireframes
+//
+//  Created by Jaymin Ding on 12/13/20.
+//
+
+import SwiftUI
+
+struct HangrRating: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HangrRating_Previews: PreviewProvider {
+    static var previews: some View {
+        HangrRating()
+    }
+}
---
+++ Wireframes/Wireframes/Order Tile/OrderTile.swift
@@ -0,0 +1,20 @@
+//
+//  OrderTile.swift
+//  Wireframes
+//
+//  Created by Jaymin Ding on 12/13/20.
+//
+
+import SwiftUI
+
+struct OrderTile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderTile_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderTile()
+    }
+}
